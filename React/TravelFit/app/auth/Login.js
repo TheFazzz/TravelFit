40a51154597ebd0c0e90566943d94da5e9e65d83
Timothyvu99 <timothyvu99@gmail.com>
2024-02-16 22:16:19 -0800

    Login appearance

--- React/TravelFit/app/auth/Login.js
+++ React/TravelFit/app/auth/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { StyleSheet, Text, TextInput, View, Image } from 'react-native';
 import { Link } from 'expo-router';
 // import * as Keychain from 'react-native-keychain'
+import { KeyboardAvoidingView } from 'native-base';
 
 export default function index() {
 
@@ -10,6 +11,12 @@ export default function index() {
   const handleLogin = async ()=> {
     const token = '';
     const username = '';
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+      setShowPassword(!showPassword);
+    }
 
     await Keychain.setPassword(username, token);
     setIsLoggedin(true);
@@ -20,32 +27,55 @@ export default function index() {
 
   return (
     <View>
-        <Image source={require('../../assets/travelfitlogo.png')}></Image>
+        <KeyboardAvoidingView style={styles.container}>
+          <Image style={styles.imageSize} source={require('../../assets/travelfitlogo.png')}></Image>
 
-        <TextInput placeholder='Username' id='username' style={styles.userinput}>
-        </TextInput> 
 
-        <TextInput placeholder='Password' id='password' style={styles.userinput}>
-        </TextInput> 
+          <TextInput placeholder='Username' id='username' style={styles.userinput}>
+          </TextInput> 
 
-        <Text onPress={handleLogin} style={styles.loginButton}>Login</Text>
+          <TextInput secureTextEntry={false} placeholder='Password' id='password' style={styles.userinput}>
+          </TextInput> 
+
+          <Text onPress={handleLogin} style={styles.loginButton}>Login</Text>
+        </KeyboardAvoidingView>
     </View>
   )
 }
 
-const styles = {
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    // flex: 1,
+    backgroundColor: 'silver',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   userinput: {
+    overflow: 'hidden',
     border: '1px solid black',
     padding: '5px',
-    borderRadius: '15px',
+    borderRadius: 15,
     marginBottom: '10px'
   },
   loginButton: {
+    overflow: 'hidden',
     backgroundColor: '#89CFF0',
-    borderRadius: '15px',
+    borderRadius: 15,
     color: 'white',
     paddingVertical: '5px',
     paddingHorizontal: '25px',
-    textAlign: 'center'
+    textAlign: 'center',
+    width: 100,
+    height: 25,
+  },
+  imageSize: {
+    // flex: 1,
+    // aspectRatio: 1.5,
+    height: 400,
+    width: 400,
+    resizeMode: 'contain',
+    justifyContent: 'center'
   }
-};
+});
