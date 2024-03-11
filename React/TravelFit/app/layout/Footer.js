1fd892e949c29e3d0e54cbafffe47d77672d42a0
Timothyvu99 <timothyvu99@gmail.com>
2024-03-11 13:56:50 -0700

    Icons implemented

--- React/TravelFit/app/layout/Footer.js
+++ React/TravelFit/app/layout/Footer.js
@@ -2,17 +2,29 @@ import React from 'react'
 import { Link } from 'expo-router'
 import { StyleSheet, Text, View } from 'react-native';
 import { styles } from '../styles';
+import { Icon } from 'native-base';
+import { MaterialCommunityIcons} from '@expo/vector-icons'
 
 export default function Footer() {
   return (
     <View style={styles.footer}>
-        <Link href={'/home'}>Home</Link>
-        <Link href={'/home/profile'}>Profile</Link>
-        <Link href={'/home/map'}>Map</Link>
+        <Link href={'/home'}>
+          <Icon as={MaterialCommunityIcons} name='home' style={{paddingTop: 10}} size='8' />
+        </Link>
+
+        <Link href={'/home/profile'}>
+          <Icon as={MaterialCommunityIcons} name='account' style={{paddingTop: 10}} size='8' />
+        </Link>
+        <Link href={'/home/map'}>
+          <Icon as={MaterialCommunityIcons} name='google-maps' style={{paddingTop: 10}} size='8' />
+        </Link>
         <Link href={{
           pathname: '/home/gymPage/[id]',
           params: { id: 6 }
-          }}>Gym Page</Link>
+          }}>
+          <Icon as={MaterialCommunityIcons} name='arm-flex' style={{paddingTop: 10}} size='8' />
+
+          </Link>
     </View>
   )
 }
