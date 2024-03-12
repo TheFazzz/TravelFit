c48e1902f9276e6529959ea9414a8ccd8c12ec93
Timothyvu99 <timothyvu99@gmail.com>
2024-03-12 00:29:02 -0700

    Exit button

--- React/TravelFit/app/layout/Header.js
+++ React/TravelFit/app/layout/Header.js
@@ -6,7 +6,7 @@ import { Button, extendTheme } from 'native-base'
 
 import { context } from '../_layout';
 
-import { Icon } from 'native-base';
+import { Icon, IconButton } from 'native-base';
 import { MaterialCommunityIcons} from '@expo/vector-icons'
 
 export default function Header() {
@@ -17,17 +17,19 @@ export default function Header() {
     <View style={styles.header}>
         <Link href={'/'} style={header.container}>
           <Icon as={MaterialCommunityIcons} name='arrow-u-left-bottom' style={{paddingTop: 10, color: '#AFE5E7'}} size='8' />
+          {/* <IconButton href={'/'} icon={<Icon as={MaterialCommunityIcons} name='arrow-u-left-bottom' style={header.back} size='8' />} /> */}
         </Link>
-        <View style={styles.exit}>
-        {searchFocus? <Button
-          onPress={() => {setSearchFocus(false)}}
+
+        <IconButton
+          icon={<Icon as={MaterialCommunityIcons} name='window-close' style={header.exit} size='8' />}
+          // onPress={() => {setSearchFocus(false)}}
           size='sm'
           color='red.100'
-          style={styles.exit}
+          style={header.exit}
+          onPress={() => {console.log('exit')}}
+
         >
-          X
-        </Button>: <></>}
-        </View>
+        </IconButton>
     </View>
   )
 }
@@ -36,12 +38,29 @@ const header = StyleSheet.create({
   container: {
     paddingTop: 50,
     color: '#FFFFFF',
+    flexDirection: 'row'
   },
   exit: {
-    paddingBottom: 10,
     position: 'absolute',
+    marginLeft: 370,
+    // right: 0,
+    // top: 26,
+    marginTop: 52,
+    // bottom: 0,
+    color: 'red',
+    width: 33,
+    height: 33,
+    backgroundColor: '#004C6D'
+  },
+  back: {
+    position: 'absolute',
+    left: 0,
     right: 0,
-    marginLeft: 300,
-    width: 10,
+    top: 0,
+    bottom: 0,
+    // width: 50,
+    // height: 50,
+    color: '#AFE5E7',
+    // backgroundColor: 'red'
   }
 })
