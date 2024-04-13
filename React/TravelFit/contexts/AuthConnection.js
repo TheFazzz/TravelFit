d613faf4dc12e9b5548a82b47c599c17e4fdbefa
Bryan Garcia <bryan.garcia.cuevas@gmail.com>
2024-04-13 15:06:26 -0700

    changes back to normal testing

--- React/TravelFit/contexts/AuthConnection.js
+++ React/TravelFit/contexts/AuthConnection.js
@@ -3,8 +3,8 @@ import { Platform } from "react-native"
 let URL
 
 if (Platform.OS == 'ios') {
-    //URL = `http://127.0.0.1:8000`
-    URL = `https://travelfit.azurewebsites.net/`
+    URL = `http://127.0.0.1:8000`
+    //URL = `https://travelfit.azurewebsites.net/`
 } else if (Platform.OS == 'android') {
     URL = `http://10.0.2.2:8000`
 }
